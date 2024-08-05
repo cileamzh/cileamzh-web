a3641ffc24360c2b61bf43a2a234bb21b31e407f
cileamzh <cileamzh@gmail.com>
2024-08-05 23:23:03 +0800

    tidy

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,3 @@
-use std::{
-    collections::HashMap,
-    io::{Read, Result, Write},
-    net::TcpStream,
-    sync::Arc,
-};
-
 pub mod httprequest;
 pub mod httpresponse;
 pub mod server;
@@ -15,47 +8,3 @@ pub use server::Server;
 
 type Handler = dyn Fn(&mut HttpRequest, &mut HttpResponse) + Send + Sync + 'static;
 type Middleware = dyn Fn(&mut HttpRequest, &mut HttpResponse) + Send + Sync + 'static;
-
-fn handle_stream(
-    mut stream: TcpStream,
-    route: Arc<HashMap<(String, String), Arc<Handler>>>,
-    middlewares: Arc<Vec<Arc<Middleware>>>,
-) -> Result<()> {
-    let req_str = read_stream_to_httpstr(&stream)?;
-    let mut req = HttpRequest::from(req_str)?;
-    let mut res = HttpResponse::new();
-
-    for middleware in middlewares.iter() {
-        middleware(&mut req, &mut res);
-    }
-
-    let key = (req.get_method().to_string(), req.get_path().to_string());
-    if let Some(handler) = route.get(&key) {
-        handler(&mut req, &mut res);
-        stream.write_all(res.get_string().as_bytes())?;
-        stream.flush()?;
-    } else {
-        res.set_body("404 Not Found");
-        res.set_header("Content-Type", "text/plain");
-        stream.write_all(res.get_string().as_bytes())?;
-        stream.flush()?;
-    }
-
-    Ok(())
-}
-
-fn read_stream_to_httpstr(mut stream: &TcpStream) -> Result<String> {
-    let mut buf = vec![0; 512];
-    let mut result = String::new();
-    loop {
-        let read = stream.read(&mut buf)?;
-        if read == 0 {
-            break;
-        }
-        result.push_str(&String::from_utf8_lossy(&buf[..read]));
-        if read < buf.len() {
-            break;
-        }
-    }
-    Ok(result)
-}
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,12 @@
-use std::{collections::HashMap, io::Result, net::TcpListener, sync::Arc, thread::spawn};
+use std::{
+    collections::HashMap,
+    io::{Read, Result, Write},
+    net::{TcpListener, TcpStream},
+    sync::Arc,
+    thread::spawn,
+};
 
-use crate::{handle_stream, Handler, HttpRequest, HttpResponse, Middleware};
+use crate::{Handler, HttpRequest, HttpResponse, Middleware};
 
 pub struct Server {
     listener: TcpListener,
@@ -67,4 +73,46 @@ impl Server {
     {
         self.add_route("POST", path, handler);
     }
+}
+fn handle_stream(
+    mut stream: TcpStream,
+    route: Arc<HashMap<(String, String), Arc<Handler>>>,
+    middlewares: Arc<Vec<Arc<Middleware>>>,
+) -> Result<()> {
+    let req_str = read_stream_to_httpstr(&stream)?;
+    let mut req = HttpRequest::from(req_str)?;
+    let mut res = HttpResponse::new();
+
+    for middleware in middlewares.iter() {
+        middleware(&mut req, &mut res);
+    }
+
+    let key = (req.get_method().to_string(), req.get_path().to_string());
+    if let Some(handler) = route.get(&key) {
+        handler(&mut req, &mut res);
+        stream.write_all(res.get_string().as_bytes())?;
+        stream.flush()?;
+    } else {
+        res.set_body("404 Not Found");
+        res.set_header("Content-Type", "text/plain");
+        stream.write_all(res.get_string().as_bytes())?;
+        stream.flush()?;
+    }
+
+    Ok(())
+}
+fn read_stream_to_httpstr(mut stream: &TcpStream) -> Result<String> {
+    let mut buf = vec![0; 512];
+    let mut result = String::new();
+    loop {
+        let read = stream.read(&mut buf)?;
+        if read == 0 {
+            break;
+        }
+        result.push_str(&String::from_utf8_lossy(&buf[..read]));
+        if read < buf.len() {
+            break;
+        }
+    }
+    Ok(result)
 }
